184a7f290316eea630ee9bc3c33544893b4f401c
Sucerquia <danielsucerquia@hotmail.com>
2026-02-23 22:10:25 +0100

    ENH: nicer data storage

--- QM_validations/script_plots/severalEPR_PYD_plotSI.py
+++ QM_validations/script_plots/severalEPR_PYD_plotSI.py
@@ -1,3 +1,5 @@
+# %%
+
 import matplotlib.pyplot as plt
 from myutils.g_values.best_fit import *
 import kimmdy_paper_theme
@@ -17,8 +19,7 @@ fieldrange = None
 
 tme = TheoMatchExpe(files,
                     experiment_file='../experiments/' +
-                                    'Kurth2023_G-band-Achilles.dat',
-                    fieldrange=fieldrange)
+                                    'Kurth2023_G-band-Achilles.dat')
 
 fig, ax = plt.subplots(2,2)
 
@@ -27,6 +28,15 @@ sp = StandardPlotter(fig=fig, ax=ax.flatten(), set_default=False)
 sp.fig.set_size_inches(kimmdy_paper_theme.double_column,
                        1.2 * kimmdy_paper_theme.single_column)
 sp.change_general_pars(kimmdy_paper_theme.default_plot_config)
+
+field = (tme.fieldexp + tme.shift)/1000
+exp = tme.intensexp
+pyd = tme.coeffs[1] * tme.intensities[1]
+dopa = tme.coeffs[0] * tme.intensities[0]
+with open('severalEPR_SIa.dat', 'w') as f:
+    f.write('# field[T]\texp\tpyd\tdopa\n')
+    for i in range(len(field)):
+        f.write(f'{field[i]:.5f}\t{exp[i]:.6E}\t{pyd[i]:.6E}\t{dopa[i]:.6E}\n')
 
 sp.plot_data((tme.fieldexp + tme.shift)/1000, tme.intensexp, pstyle='-',
              color_plot='black', data_label='EPR spectrum')
@@ -47,9 +57,17 @@ sp.ax[0].legend()
 
 # Second
 tme = TheoMatchExpe(files,
-                    experiment_file='../experiments/Kurth2023_G-band-Tail.dat',
-                    fieldrange=fieldrange)
+                    experiment_file='../experiments/Kurth2023_G-band-Tail.dat')
 tme.fit_shifting(tme.files, shifting_width=5)
+
+field = (tme.fieldexp + tme.shift)/1000
+exp = tme.intensexp
+pyd = tme.coeffs[1] * tme.intensities[1]
+dopa = tme.coeffs[0] * tme.intensities[0]
+with open('severalEPR_SIb.dat', 'w') as f:
+    f.write('# field[T]\texp\tpyd\tdopa\n')
+    for i in range(len(field)):
+        f.write(f'{field[i]:.5f}\t{exp[i]:.6E}\t{pyd[i]:.6E}\t{dopa[i]:.6E}\n')
 
 sp.plot_data((tme.fieldexp + tme.shift)/1000, tme.intensexp, pstyle='-',
              color_plot='black', data_label='EPR spectrum', ax=1)
@@ -71,9 +89,17 @@ sp.ax[1].legend()
 # Third
 tme = TheoMatchExpe(files,
                     experiment_file='../experiments/' +
-                                    'Kurth2023_G-band-Meniscus.dat',
-                    fieldrange=fieldrange)
+                                    'Kurth2023_G-band-Meniscus.dat')
 tme.fit_shifting(tme.files, shifting_width=5)
+
+field = (tme.fieldexp + tme.shift)/1000
+exp = tme.intensexp
+pyd = tme.coeffs[1] * tme.intensities[1]
+dopa = tme.coeffs[0] * tme.intensities[0]
+with open('severalEPR_SIc.dat', 'w') as f:
+    f.write('# field[T]\texp\tpyd\tdopa\n')
+    for i in range(len(field)):
+        f.write(f'{field[i]:.5f}\t{exp[i]:.6E}\t{pyd[i]:.6E}\t{dopa[i]:.6E}\n')
 
 sp.plot_data((tme.fieldexp + tme.shift)/1000, tme.intensexp, pstyle='-',
              color_plot='black', data_label='EPR spectrum', ax=2)
@@ -94,9 +120,17 @@ sp.ax[2].legend()
 
 # Fourth
 tme = TheoMatchExpe(files,
-                    experiment_file='../experiments/Zapp2020.dat',
-                    fieldrange=fieldrange)
+                    experiment_file='../experiments/Zapp2020.dat')
 tme.fit_shifting(tme.files, shifting_width=20)
+
+field = (tme.fieldexp + tme.shift)/1000
+exp = tme.intensexp
+pyd = tme.coeffs[1] * tme.intensities[1]
+dopa = tme.coeffs[0] * tme.intensities[0]
+with open('severalEPR_SId.dat', 'w') as f:
+    f.write('# field[T]\texp\tpyd\tdopa\n')
+    for i in range(len(field)):
+        f.write(f'{field[i]:.5f}\t{exp[i]:.6E}\t{pyd[i]:.6E}\t{dopa[i]:.6E}\n')
 
 sp.plot_data((tme.fieldexp + tme.shift)/1000, tme.intensexp,
              pstyle='-', color_plot='black', data_label='EPR spectrum', ax=3)
